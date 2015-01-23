d07bca2aa7a71e4a9f9d320aa259cbcfe7c5b7ec
fominykh <fominykh@fominykhds.(none)>
2015-01-23 11:31:05 +0200

    list, create add

--- js/script.js
+++ js/script.js
@@ -1,24 +1,58 @@
-var BooklistApp = angular.module('app', ['ngRoute']);
+var BooklistApp = angular.module('app', ['ngRoute', 'app.services']);
 
 BooklistApp.config(function($routeProvider) {
   $routeProvider.
+    when('/', {
+      templateUrl: 'views/books.html',
+      controller: 'BookListCtrl'
+    }).
     when('/books', {
       templateUrl: 'views/books.html',
       controller: 'BookListCtrl'
     }).
-    // when('/books/:phoneId/edit', {
-    //   templateUrl: 'views/edit.html',
-    //   controller: 'BookEditCtrl'
-    // }).
-    // when('/books/create', {
-    //   templateUrl: 'views/create.html',
-    //   controller: 'BookCreateCtrl'
-    // }).
+    when('/create', {
+      templateUrl: 'views/create.html',
+      controller: 'BookCreateCtrl'
+    }).
+    when('/books/:bookId', {
+      templateUrl: 'views/book.html',
+      controller: 'BookCtrl'
+    }).
     otherwise({
       redirectTo: '/'
     });
 });
 
-function BookListCtrl($scope) {
-  $scope.books = "tyghj";
-}
+BooklistApp.controller("BookListCtrl", ['$scope', 'Books', function($scope, Books){
+  Books.getAll().success(function(data) {
+    $scope.books = data.results;
+    console.log(data.results);
+  });
+}]);
+
+BooklistApp.controller("BookCtrl", ['$scope', '$routeParams', 'Books', function($scope, $routeParams, Books){
+  Books.get( $routeParams.bookId ).success(function(data) {
+    $scope.book = data;
+    console.log(data);
+  });
+}]);
+
+BooklistApp.controller("BookCreateCtrl", ['$scope', '$window', 'Books', function($scope, $window, Books){
+  var self = this;
+  $scope.create = function() {
+      Books.create({ name : $scope.book.name, author : $scope.book.author, publish_year : $scope.book.publish_year }).success(function(data) {
+          // $state.go('todos');
+          $window.location.href = '#/books';
+      });
+  }
+  $scope.isClean = function() {
+    return angular.equals(self.original, $scope.book);
+  }
+}]);
+
+// BooklistApp.controller("BookEditCtrl", ['$scope', 'Books', function($scope, Books){
+//   Books.getAll().success(function(data){
+//     $scope.books = data.results;
+//     console.log(data.results);
+//   });
+// }]);
