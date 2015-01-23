2f001118f464862d026de713affd48e394b0f3e6
fominykh <fominykh@fominykhds.(none)>
2015-01-23 12:51:38 +0200

    bug fix

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ BooklistApp.controller("BookCtrl", ['$scope', '$routeParams', '$window', 'Books'
   $scope.save = function(){
     Books.edit( $routeParams.bookId, $scope.book ).success(function(data) {
       $scope.book = data;
-      console.log('book saveed 1');
+      console.log('book saveed');
       $window.location.href = '#/books';
     });
   }
@@ -56,14 +56,8 @@ BooklistApp.controller("BookCtrl", ['$scope', '$routeParams', '$window', 'Books'
 BooklistApp.controller("BookCreateCtrl", ['$scope', '$window', 'Books', function($scope, $window, Books){
   $scope.create = function() {
       Books.create({ name : $scope.book.name, author : $scope.book.author, publish_year : parseInt($scope.book.publish_year, 10) }).success(function(data) {
+          console.log('book created');
           $window.location.href = '#/books';
       });
   }
 }]);
-
-// BooklistApp.controller("BookEditCtrl", ['$scope', 'Books', function($scope, Books){
-//   Books.getAll().success(function(data){
-//     $scope.books = data.results;
-//     console.log(data.results);
-//   });
-// }]);
